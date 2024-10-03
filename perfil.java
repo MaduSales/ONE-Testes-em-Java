e3cb4b64e05e8798059b443ad627bdfb7c7adcb1
Maria Eduarda de Sales Miranda <166547195+MaduSales@users.noreply.github.com>
2024-10-03 11:05:05 -0300

    Add files via upload

---
+++ perfil.java
@@ -0,0 +1,14 @@
+
+/*
+Online Java - IDE, Code Editor, Compiler
+
+Online Java is a quick and easy tool that helps you to build, compile, test your programs online.
+*/
+
+public class Perfil
+{
+    public static void main(String[] args) {
+        System.out.println("Olá");
+        System.out.println("Tudo bem?");
+    }
+}

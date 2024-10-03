0ad2ac8e3a2695489420e819f5edb14a3b35e52e
Maria Eduarda de Sales Miranda <166547195+MaduSales@users.noreply.github.com>
2024-10-03 11:09:25 -0300

    Add files via upload

---
+++ estudos.java
@@ -0,0 +1,10 @@
+
+public class Estudos
+{
+    public static void main(String[] args){
+        System.out.println("Anotações de estudos:");
+        System.out.println("- Java, atualmente, é uma plataforma da Oracle;");
+        System.out.println("- É necessário instalar o JDK (kit para desenvolvimento) e o IntelliJ (IDEA) de maneira separada;");
+        System.out.println("- O Java utiliza uma JVM para compilar o código para ser interpretado por todos os SO;");
+    }
+}

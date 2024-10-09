3815ced7900a497d73713d76bf733cc0f707c3c6
Maria Eduarda de Sales Miranda <166547195+MaduSales@users.noreply.github.com>
2024-10-09 11:03:00 -0300

    Add files via upload

---
+++ ScreenMatch-teste1/src/main/java/Main.java
@@ -0,0 +1,16 @@
+
+
+public class Main {
+  public static void main(String[] args) {
+  System.out.println("Esse é o Screen Match!");
+
+  System.out.println("Filme: Top Gun: Maverick");
+  /*int ano;
+  ano = 2022;*/
+  int anoDeLancamento = 2022;
+  System.out.println("Ano de lançamento: " + anoDeLancamento);
+
+  boolean incluidoNoPlano = true;
+    double notaDoFilme = 
+  }
+}

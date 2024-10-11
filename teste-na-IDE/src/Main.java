73ad0163f364971d12d748217766623e68dadeb4
Maria Eduarda de Sales Miranda <166547195+MaduSales@users.noreply.github.com>
2024-10-11 09:07:09 -0300

    Add files via upload

---
+++ teste-na-IDE/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("""
+                ========================================================
+                Filme: Top Gun Maverick.
+                
+                Sinopse: Sequência do clássico dos anos 80 sem sentido.
+                ========================================================
+                """);
+    }
+}

ac9031353981de29f132decf0a04fab4f3df8295
Maria Eduarda de Sales Miranda <166547195+MaduSales@users.noreply.github.com>
2024-10-17 15:12:12 -0300

    Add files via upload

---
+++ java/Main.java
@@ -0,0 +1,13 @@
+
+public class Main{
+  public static void main(String[] args){
+
+    Filme meuFilme = new Filme();
+
+    meuFilme.nome = "Top Gun";
+    meuFilme.anoDeLancamento = 1986;
+
+    System.out.println(meuFilme.nome);
+    System.out.println(meuFilme.anoDeLancamento);
+  }
+}
